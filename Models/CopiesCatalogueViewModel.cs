f78bf97fbff1da582c8c8ad5c2f01444e290a19f
Lena E <elena.evdokimova@softwire.com>
2021-07-07 22:12:40 +0100

    Created Book Copies Views and Controlles

---
+++ Models/CopiesCatalogueViewModel.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace Bookish.Models
+{
+    public class CopiesCatalogueViewModel
+    {
+        public List<BookCopyViewModel> Copies { get; set; }
+    }
+}
---
+++ Queries/BookQuery.cs
@@ -0,0 +1,26 @@
+using Bookish.DbModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace Bookish.Queries
+{
+    public class BookQuery
+    {
+        //public static List<BookDbModel>  Query()
+        //{
+        //    var context = new LibraryContext();
+
+        //    var books = context.Books
+        //                      .FromSqlRaw("Select * from Books where Id = 1").ToListAsync();
+        //    books.Start();
+        //    books.Wait();
+
+        //    Console.WriteLine(books.Result[0].Title);
+
+        //    return books.Result;
+
+
+        //}
+    }
+}
